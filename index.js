1c01f2ce35acfdd3d5c35efe9c3754ce91b09c3d
Anton <krivelo2017@mail.ru>
2023-10-17 16:20:25 +0300

    add index.js quest 1,2,3 cikl

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 console.log('abc');
+
+//задачи на циклы 1
+// Место для первой задачи
+function firstTask() {
+    for (let i = 5; i <= 10; i++) {
+        console.log(i);
+    }
+}
+console.log(firstTask());
+
+// задача 2
+function secondTask() {
+    for (let i = 20; i >= 10; i--) {
+         if (i === 13) return;
+         console.log(i);
+      
+     }
+ }
+ 
+ // Место для третьей задачи
+ function thirdTask() {
+     for (let i = 2; i <= 10; i++) {
+         if (i % 2 === 0) {
+             console.log(i);
+         }
+     }
+ }
+ 
+
+//массивы и псевдомассивы 
